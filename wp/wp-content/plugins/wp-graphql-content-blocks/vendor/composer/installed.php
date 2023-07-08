51cb4010b27c4d249d4b8149560fc5bfdb4b7ef8
TalesHs <webtailss@gmail.com>
2023-07-08 20:54:03 -0300

    Install plugins and setup nuxt

---
+++ wp/wp-content/plugins/wp-graphql-content-blocks/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'wpengine/wp-graphql-content-blocks',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '9f9f1ad96938e87b57ab9de6aa0d9641735be0b6',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'imangazaliev/didom' => array(
+            'pretty_version' => '2.0.1',
+            'version' => '2.0.1.0',
+            'reference' => '50fa6595d14f22c0c984efed5c818485cf548136',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../imangazaliev/didom',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'wpengine/wp-graphql-content-blocks' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '9f9f1ad96938e87b57ab9de6aa0d9641735be0b6',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
